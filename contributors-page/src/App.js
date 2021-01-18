70659c05ec2f482a32998d7270d03c6686e08820
Divya Garg <divyagarg2601@gmail.com>
2021-01-18 19:48:33 +0530

    60% work completed

---
+++ contributors-page/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Contributors from './Components/Contributors.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Contributors />
+    
+    </div>
+  );
+}
+
+export default App;
